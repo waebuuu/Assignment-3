9d2f2b7f1fe56405a12eb9972b8257fd1b63798a
waebuuu <zahra.mirza@ontariotechu.net>
2025-11-19 23:30:07 -0500

    fixed smt w the index

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,20 +1,23 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
 /* GET home page. */
-router.get('/home', function(req, res, next) {
-  res.render('index', { title: 'Home' });
+router.get("/home", function (req, res, next) {
+  res.render("index", {title: "Home"});
+});
+/* GET home page. */
+router.get("/", function (req, res, next) {
+  res.render("index", {title: "Home"});
 });
 
 /* GET About page. */
-router.get('/about', function(req, res, next) {
-  res.render('about', { title: 'About us' });
+router.get("/about", function (req, res, next) {
+  res.render("about", {title: "About us"});
 });
 
-
 /* GET Contact page. */
-router.get('/contact', function(req, res, next) {
-  res.render('contact', { title: 'Contact us' });
+router.get("/contact", function (req, res, next) {
+  res.render("contact", {title: "Contact us"});
 });
 
 module.exports = router;
